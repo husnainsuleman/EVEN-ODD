3eef990f888140b9ab35c55ef4f2e6fc9ace8af5
Muhammad Husnain <51457254+husnainsuleman@users.noreply.github.com>
2021-07-16 00:05:27 -0800

    Update EVEN-ODD.py

--- EVEN-ODD.py
+++ EVEN-ODD.py
@@ -1,8 +1,9 @@
-while True:
-    im=(int(input("Wrtie:"))) 
-    if im%2 == 0:
-        print("Even")
-    elif im %2 != 0:
-        print("ODD")
+def my_name(number):
+    if number %2 == 0  :
+        print('This is Even Number')
+    elif number %2 != 0:
+        print('This is ODD Number')
     else:
-        print("nothing")
+        print('Write Grater Then Zero 0')
+my_name(6)
+  
